37034623298090421fdbae2b34d7150b7655c574
Annvea Chung <annveabchung@gmail.com>
2025-05-23 00:02:21 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </Provider>
-);
+    </Provider>
+  </React.StrictMode>,
+)
